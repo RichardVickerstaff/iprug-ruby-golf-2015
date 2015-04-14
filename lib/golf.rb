e15ef19c6be6efcc5a3892ac4fb4799991fbe11e
Richard Vickerstaff <richard.vickerstaff@bt.com>
2015-04-14 22:02:24 +0100

    5 works

--- lib/golf.rb
+++ lib/golf.rb
@@ -7,17 +7,20 @@ class Golf
 
   def hole_2 w
     v=%w[a e i o u A E I O U]
-    w.split.map{|l|[l.each_char.select{|f|v.include? f}.size,l.each_char.reject{|f|v.include? f}.size,l]}
-      .sort.map{|h|h[2]}.join" "
+    w.split.map{|l|[l.each_char.select{|f|v.include? f}.size,l.each_char.reject{|f|v.include? f}.size,l.downcase,l]}
+      .sort.map{|h|h[3]}.join" "
   end
 
-  def hole_3
+  def hole_3 n
+    (1..n).select{|m|m if n%m==0}
   end
 
-  def hole_4
+  def hole_4 x,y
   end
 
-  def hole_5
+  def hole_5 a,b
+    a=[a,b].map{|i|i.upcase.gsub(/[ ()!-]/,'').each_char.sort}
+    a[1]==a[0]
   end
 
   def hole_6
