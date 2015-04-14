f1e17c161f05c30d63af342e719df58b49bdc819
Richard Vickerstaff <richard.vickerstaff@bt.com>
2015-04-14 21:27:57 +0100

    2 almost done

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,8 +1,14 @@
 class Golf
-  def hole_1
+  def hole_1 c
+    s=c.upcase==c ? 'A':'a'
+    i=0
+    (s..c).map{|l|l*i+=1}.join"\n"
   end
 
-  def hole_2
+  def hole_2 w
+    v=%w[a e i o u A E I O U]
+    w.split.map{|l|[l.each_char.select{|f|v.include? f}.size,l.each_char.reject{|f|v.include? f}.size,l]}
+      .sort.map{|h|h[2]}.join" "
   end
 
   def hole_3
