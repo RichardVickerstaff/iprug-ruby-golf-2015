6432d8e5945369646e0344d89e90c9b5a17a2a6f
Richard Vickerstaff <richard.vickerstaff@bt.com>
2015-04-15 12:03:10 +0100

    Make 2 crazy

--- lib/golf.rb
+++ lib/golf.rb
@@ -4,12 +4,6 @@ class Golf
     ((c>?Z??a:?A)..c).map{|l|l*i+=1}.join"\n"
   end
 
-  def hole_2 w
-    v=%w[a e i o u A E I O U]
-    w.split.map{|l|[l.each_char.count{|f|v.include? f},l.each_char.count{|f|!v.include? f},l.downcase,l]}
-      .sort.map{|h|h[3]}.join" "
-  end
-
   def hole_3 n
     (1..n).select{|m|n%m==0}
   end
@@ -41,5 +35,9 @@ class Golf
 
   def hole_9 h
     h.values.flat_map(&:values).inject{|i,j|i.map{|k,v|[k,v+j[k]]}}.to_h
+  end
+
+  def hole_2 s
+    s.split.sort_by{|w|w.upcase.chars.partition{|x|x !~/AEIOU/}.map(&:length)<<w.upcase}.join" "
   end
 end
